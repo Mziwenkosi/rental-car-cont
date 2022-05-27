7b19576de7553746c9bc6a41a4a580cf1bf64119
Mziwenkosi <59651600+Mziwenkosi@users.noreply.github.com>
2022-05-27 16:08:24 +0200

    Add files via upload
    home, Rent With 3 Easy Steps, BEST SERVICES andABOUT US

---
+++ car rental/main.js
@@ -0,0 +1,52 @@
+<div class="box-img">
+<img src="img/car1.jpg" alt="">
+</div>
+<p>2017</p>
+<h3>2018 Honda Civic</h3>
+<h2>R3500 | R3600 <span>/month</span></h2>
+<a href="#" class="btn">Rent Now</a>
+
+
+<div class="box-img">
+<img src="img/car2.jpg" alt="">
+</div>
+<p>2017</p>
+<h3>2018 Honda Civic</h3>
+<h2>R3500 | R3600 <span>/month</span></h2>
+<a href="#" class="btn">Rent Now</a>
+
+
+<div class="box-img">
+<img src="img/car3.jpg" alt="">
+</div>
+<p>2017</p>
+<h3>2018 Honda Civic</h3>
+<h2>R3500 | R3600 <span>/month</span></h2>
+<a href="#" class="btn">Rent Now</a>
+
+
+<div class="box-img">
+<img src="img/car4.jpg" alt="">
+</div>
+<p>2017</p>
+<h3>2018 Honda Civic</h3>
+<h2>R3500 | R3600 <span>/month</span></h2>
+<a href="#" class="btn">Rent Now</a>
+
+
+<div class="box-img">
+<img src="img/car5.jpg" alt="">
+</div>
+<p>2017</p>
+<h3>2018 Honda Civic</h3>
+<h2>R3500 | R3600 <span>/month</span></h2>
+<a href="#" class="btn">Rent Now</a>
+
+
+<div class="box-img">
+<img src="img/car6.jpg" alt="">
+</div>
+<p>2017</p>
+<h3>2018 Honda Civic</h3>
+<h2>R3500 | R3600 <span>/month</span></h2>
+<a href="#" class="btn">Rent Now</a>
